ecc8883ac1c9eb299d358b7c5c680fc856f6d4c8
patrick_mugo <patrickkinyua2004@gmail.com>
2025-09-13 08:50:30 +0300

    create simple python script for basic operations

---
+++ basic-operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Addition
+sum_result = number1 + number2
+print("Sum of",  number1, "and", number2, "is:", sum_result)
+# Subtraction
+difference_result = number1 - number2
+print("Difference of",  number1, "and", number2, "is:", difference_result)
+# Multiplication
+product_result = number1 * number2
+print("Product of",  number1, "and", number2, "is:", product_result)
